67b865a9c2b5572dd02d3ac800cd53a0ad725df2
aminulislam67 <md.aminul.islam2@g.bracu.ac.bd>
2022-04-15 16:42:40 +0600

    changed userlogin

--- admin/View/userlogin.php
+++ admin/View/userlogin.php
@@ -1,27 +1,7 @@
 <?php
 include("../../configuration/config.php");?>
-<?php
+<?php include("../Controller/userlogincontrol.php")?>
 
-$login = new Login();
-
-if(isset($_POST["submit"])){
-  $result = $login->login($_POST["usernameemail"], $_POST["password"]);
-
-  if($result == 1){
-    $_SESSION["login"] = true;
-    $_SESSION["id"] = $login->idUser();
-    header("Location: userindex.php");
-  }
-  elseif($result == 10){
-    echo
-    "<script> alert('Wrong Password'); </script>";
-  }
-  elseif($result == 100){
-    echo
-    "<script> alert('User is Not Registered yet'); </script>";
-  }
-}
-?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
